1f6ae79e1192d3d34e9e5848f5fcf500ebca6804
luissanr <luis.sanchez2294@gmail.com>
2021-03-31 10:48:39 -0600

    Add and remove to Wishlist

---
+++ src/test/java/myTests/RemoveWishlistTest.java
@@ -0,0 +1,59 @@
+package myTests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import pages.AuthenticationPage;
+import pages.MainPage;
+import pages.MyAccountPage;
+import pages.MyWishlistPage;
+
+public class RemoveWishlistTest {
+
+    WebDriver driver;
+
+	@BeforeTest
+	public void setBaseURL() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./src/resources/" + ( System.getProperty("os.name").toLowerCase().contains("mac") ? "chromedriver" : "chromedriver.exe" ));
+		driver = new ChromeDriver();
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+	}
+
+    @Test
+	public void signIn()  throws InterruptedException {
+
+		MainPage mainPage = PageFactory.initElements(driver, MainPage.class);
+		AuthenticationPage authenticationPage = PageFactory.initElements(driver, AuthenticationPage.class);
+		MyAccountPage accountPage = PageFactory.initElements(driver, MyAccountPage.class);
+		MyWishlistPage wishlistPage = PageFactory.initElements(driver, MyWishlistPage.class);
+
+		// Sign In
+		mainPage.openSignInPage();
+		authenticationPage.signIn("testing1@gmail.com", "equipo4");
+
+		// Open Wishlist page
+		accountPage.openMyWishlistPage();
+
+		// Create new Wishlist
+		// wishlistPage.createWishlist("A Wishlist Test");
+
+        // Remove Wishlist
+        wishlistPage.clickRemoveWishlist();
+
+		driver.switchTo().alert().accept();
+		Thread.sleep(2000);
+		
+	}
+
+    @AfterTest
+	public void endSession() {
+		driver.quit();
+	}
+    
+}
